4ae19c28c71337bad00179037a1b7c76bd135ff0
Rene Schwarz <rene.schwarz@student.uibk.ac.at>
2022-02-16 17:33:51 +0100

    added the reading logic

--- C_Projects/main.cpp
+++ C_Projects/main.cpp
@@ -22,44 +22,55 @@ int main (int argc, char *argv[]){
     
     double *data = newRead->getData();
     
+    printf("%d\n", newRead->numberInput());
+    
     for(int i = 0; i < newRead->numberInput(); i++) {
     	printf("%lf\n", data[i]);
     }
 
     // set parameters
-    int n    = 30;
-    int N    = n*n;
-    double L = 1.;
+    int Ninit    = data[0];
+    int Nend = data[1];
+    int Nincr = data[2];
+    double L = data[3];
 
     double sigma = 1.0;
     double phi   = 0.2;
 
-    double D_rot = 0.1;
-    double v     = 0.03;
-    double R     = 0.5;
+    double D_rotInit = data[11];
+    double D_rotEnd = data[12];
+    double D_rotIncr = data[13];
+    double vInit     = data[8];
+    double vEnd = data[9];
+    double vIncr = data[10];
+    double RInit     = data[5];
+    double REnd = data[6];
+    double RIncr = data[7];
 
     //simulation parameters
-    double dt = 1.; // 0.0001; //time-step length
+    double dt = data[4]; // 0.0001; //time-step length
     int Neq   = 2000; // equilibration time
-    int Nsim  = 1000; // simulation time
-    int Nsave = 10; // how often printed
+    int Nsim  = data[14]; // simulation time
+    int Nsave = data[15]; // how often printed
 
     // initialize random number generator
     srand (12345);
 
-    // settings for directory structure - program automatically creates a sensible directory structure
-    std::string dir_name_phys_params = "N_" + std::to_string(N) + "_L_" + std::to_string(L) + "_v_" + std::to_string(v) + "_R_" + std::to_string(R) + "_D_" + std::to_string(D_rot);
-    std::string dir_name_sim_params  = "Neq_" + std::to_string(Neq) + "_Nsim_" + std::to_string(Nsim) + "_dt_" + std::to_string(dt);
-    std::string dir_name             = "../simulation_results/" + dir_name_phys_params + "/"+ dir_name_sim_params;
-    std::string save_dir_name        = "mkdir -p " + dir_name; // ../simulation_results/test";
-    const int dir_err                = system(save_dir_name.c_str());
+    for(int N = Ninit; N < Nend; N += Nincr) {
+	    // settings for directory structure - program automatically creates a sensible directory structure
+	    std::string dir_name_phys_params = "N_" + std::to_string(Ninit) + "_L_" + std::to_string(L) + "_v_" + std::to_string(v) + "_R_" + std::to_string(R) + "_D_" + std::to_string(D_rot);
+	    std::string dir_name_sim_params  = "Neq_" + std::to_string(Neq) + "_Nsim_" + std::to_string(Nsim) + "_dt_" + std::to_string(dt);
+	    std::string dir_name             = "../simulation_results/" + dir_name_phys_params + "/"+ dir_name_sim_params;
+	    std::string save_dir_name        = "mkdir -p " + dir_name; // ../simulation_results/test";
+	    const int dir_err                = system(save_dir_name.c_str());
 
-    // create the class and the initial configuration
-    class Vicsek *system;
-    system = new Vicsek(sigma, N, phi, D_rot, v, R, dt, Nsim, Nsave, &dir_name[0]);
+	    // create the class and the initial configuration
+	    class Vicsek *system;
+	    system = new Vicsek(sigma, Ninit, phi, D_rot, v, R, dt, Nsim, Nsave, &dir_name[0]);
 
-    // system->md_equilibration(Neq); // Equilibration
-    system->run_simulation(); // Simulation
+	    // system->md_equilibration(Neq); // Equilibration
+	    system->run_simulation(); // Simulation
+    }
 
     return 0;
 }
